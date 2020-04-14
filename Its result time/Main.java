750f33dca248943536ce6a5a32b9de382bd09fa4
Ritty Thomas <37534943+rittythomas@users.noreply.github.com>
2020-04-14 22:20:10 +0530

    Test code pushed to repository.

---
+++ Its result time/Main.java
@@ -0,0 +1,7 @@
+#include<iostream>
+int main(){
+  int id,mark;
+  std::cin>>id;
+  std::cin>>mark;
+  std::cout<<"Id : "<<id<<"\nTotalmarks : "<<mark;
+}
---
+++ Naming  Ceremony/Main.java
@@ -0,0 +1,6 @@
+#include<iostream>
+int main(){
+  char name[10];
+  std::cin>>name;
+  std::cout<<name;
+}
